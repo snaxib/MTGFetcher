3e9792bba61123ed838f0d66c4d0e95902ff6f96
Harry <harry@fakku.net>
2016-01-22 00:47:08 -0800

    Actually added the template HTTP stuff

--- cardFetcher.py
+++ cardFetcher.py
@@ -1,6 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import socket
+import sys
 
+HOST = ''
+PORT = 8888
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
 
 parsedCardName = ''
 cardImg = ''
